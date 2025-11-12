8cbe3c39b5ffce2c7568d6bbdb111df99f1b5bd2
Faisal Almail <faisalyounis90@gmail.com>
2025-11-12 15:46:07 +0300

    Building the form inputs

---
+++ main.js
@@ -0,0 +1,4 @@
+const settingsBtn = document.getElementById("settings-btn")
+const settingsModal = document.getElementById("settings-modal")
+
+settingsBtn.addEventListener("click", function () { settingsModal.style.display = "block" })
