05d395eb4d0a187527db8bab218e11345c3dee98
Faisal Almail <faisalyounis90@gmail.com>
2025-11-13 13:18:34 +0300

    Modal style finished, wiring now

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const settingsBtn = document.getElementById("settings-btn")
 const settingsModal = document.getElementById("settings-modal")
+const timer = document.getElementById("timer")
+const pomodoroSetting = document.getElementById("pomodoro")
 
+
+//DEFAULT values
+let pomodoro = 23
+let shortBreak = 5
+let longBreak = 15
+let color = 'red'
+let font = 1
+let seconds
+
+//opening and closing modal
 settingsBtn.addEventListener("click", function () { settingsModal.style.display = "block" })
+function closeModal (){
+    settingsModal.style.display = "none"
+}
+
+//Loading values
+timer.innerText = pomodoro + ":00"
+pomodoroSetting.value = pomodoro
